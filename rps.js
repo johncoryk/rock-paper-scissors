e82db0a63bb27d23df31b51ecc84bb12b57808a2
Cory Kelley <coryk0789@gmail.com>
2019-10-24 21:10:24 -0400

    Updating with image assets

---
+++ rps.js
@@ -0,0 +1,12 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rockChoice_div = document.getElementById("rock-choice");
+const paperChoice_div = document.getElementById("paper-choice");
+const scissorsChoice_div = document.getElementById("scissors-choice");
+
+
+
