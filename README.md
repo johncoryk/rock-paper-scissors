ada471a7193ea71676eb169c4686a874f15ecbbf
johncoryk <coryk0789@gmail.com>
2019-10-27 18:34:59 -0400

    Finalized version of rock paper scissors game with updated README.md file as 10.27.19

---
+++ README.md
@@ -0,0 +1,11 @@
+Rock, paper, scissors game assigned by The Odin Project.
+
+10.27.19
+
+I had a lot of fun making this game! It's by far the most challenging and involved coding project I've done up to this point, and I'll admit, I did have to loosely follow a tutorial online to get through it. Though it wasn't completely "out of my head" I learned a lot, which is why it was assigned (I'm assuming).
+
+I went back in and challenged myself to create a reset button from scratch, I'm proud to say it works exactly how I planned for it to. It's very true that making your own projects is the best way to learn this type of skill (html, css, js, etc). I'm already seeing myself improve!
+
+Thanks for reading, I may update the color palette and icons later on this project.
+
+-jck
--- rps.js
+++ rps.js
@@ -3,10 +3,11 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rockChoice_div = document.getElementById("rock-choice");
 const paperChoice_div = document.getElementById("paper-choice");
 const scissorsChoice_div = document.getElementById("scissors-choice");
+const resetBox_div = document.getElementById("reset-box");
 
 function getComputerChoice() {
   const choices = ["rock-choice", "paper-choice", "scissors-choice"];
@@ -14,19 +15,35 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
-function win(userInput, computerInputer) {
+function convertToWord(letter) {
+  if (letter === "rock-choice") return "Rock";
+  if (letter === "paper-choice") return "Paper";
+  return "Scissors";
+  }
+
+function win(userInput, computerInput) {
+  const userInput_div = document.getElementById(userInput);
   userScore++;
   userScore_span.innerHTML = userScore;
-  result_div.innerHTML = "You won!";
+  result_p.innerHTML = `${convertToWord(userInput)} beats ${convertToWord(computerInput)}. You win!`;
+  document.getElementById(userInput).classList.add("greenglow");
+  setTimeout(() => userInput_div.classList.remove("greenglow"), 400);
 }
 
-function lose() {
+function lose(userInput, computerInput) {
+  const userInput_div = document.getElementById(userInput);
   computerScore++;
   computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(computerInput)} beats ${convertToWord(userInput)}. You lost!`;
+  document.getElementById(userInput).classList.add("redglow");
+  setTimeout(() => userInput_div.classList.remove("redglow"), 400);
 }
 
-function draw() {
-  
+function draw(userInput) {
+  const userInput_div = document.getElementById(userInput);
+  result_p.innerHTML = `You both picked ${convertToWord(userInput)}. It's a draw.`;
+  document.getElementById(userInput).classList.add("greyglow");
+  setTimeout(() => userInput_div.classList.remove("greyglow"), 400);
 }
 
 function game(userChoice) {
@@ -45,9 +62,17 @@ function game(userChoice) {
     case "scissors-choicescissors-choice":
     case "rock-choicerock-choice":
     case "paper-choicepaper-choice":
-      draw();
+      draw(userChoice);
       break;
   }
+}
+
+function reset() {
+  userScore = 0;
+  userScore_span.innerHTML = userScore;
+  computerScore = 0;
+  computerScore_span.innerHTML = userScore;
+  result_p.innerHTML = "You reset the game!";
 }
 
 game();
@@ -64,6 +89,10 @@ function main() {
   scissorsChoice_div.addEventListener('click', function() {
     game("scissors-choice");
   })
+  resetBox_div.addEventListener('click', function() {
+    reset();
+  })
+
 }
 
 main()
